bfe8cac0b63a9bcb69dcb5302f547267e1c11649
Adelia <adelgants@gmail.com>
2020-08-25 02:27:35 +0300

    fix uppercase in translateToRussian

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 let check = document.querySelector('.checkbox');
 let newtext = '';
 const translit = {
-  'а': 'a',  'б': 'b',  'в': 'v',  'г': 'g',  'д': 'd', 
-  'е': 'e',  'ё': 'yo', 'ж': 'zh', 'з': 'z',  'и': 'i',
-  'й': 'y',  'к': 'k',  'л': 'l',  'м': 'm',  'н': 'n',  'о': 'o', 
-  'п': 'p',  'р': 'r',  'с': 's',  'т': 't',  'у': 'u',  'ф': 'f', 
-  'х': 'h',  'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'sch','ъ': "'",
-  'ы': 'i',  'ь': "'",  'э': 'e',  'ю': 'u',  'я': 'ya', 
+  'а': 'a',  'б': 'b',  'в': 'v',   'г': 'g',  'д': 'd', 
+  'е': 'e',  'ё': 'yo', 'ж': 'zh',  'з': 'z',  'и': 'i',
+  'й': 'y',  'к': 'k',  'л': 'l',   'м': 'm',  'н': 'n',  
+  'о': 'o',  'п': 'p',  'р': 'r',   'щ': 'sch', 'ш': 'sh', 
+  'с': 's',  'ц': 'ts', 'т': 't',   'у': 'u',  'ф': 'f',  
+  'х': 'h',  'ч': 'ch', 'ъ': "'",   'ы': 'i',  'ь': "'",  
+  'э': 'e',  'ю': 'u',  'я': 'ya', 
   '"': '«',  '»': '"',  '–': '-',  '—': '-',  '№': '#',  '`': "'"
 };
 
 document.addEventListener('keyup', function (event) {
-  document.querySelector('.symbols__number').innerHTML = document.querySelector('.message').value.length;
+  message = document.querySelector('.message').value;
+  document.querySelector('.symbols__number').innerHTML = message.length;
 });
 
 check.addEventListener('change', function (event) {
@@ -34,21 +36,33 @@ function translatetoEnglish(text) {
         char = translit[text.charAt(i).toLowerCase()].toUpperCase();
         newtext += char;
       }
-      catch { newtext += text.charAt(i); }
+      catch (err) { newtext += text.charAt(i); }
     }
   }
 }
 
 function translatetoRussian(text) {
+  skip = 0;
   for (var i = 0; i < text.length; i++) {
+    if (skip > 0) { 
+      skip=skip-1;
+      continue; }
+    char = '';
     for (var key in translit) {
-      switch (translit[key]) {
-        case text.charAt(i) + text.charAt(i+1) + text.charAt(i+2): newtext += key;
-        break;
-        case text.charAt(i) + text.charAt(i+1): newtext += key;
-        break;
-        case text.charAt(i): newtext += key; break;
-      }
+      if (char == '') {
+        one = text.charAt(i).toLowerCase();
+        two = one + text.charAt(i + 1);
+        three = one + text.charAt(i + 1) + text.charAt(i + 2);
+        switch (translit[key]) {
+          case three: char = key; skip = 2;
+          break;
+          case two: char = key;
+          skip = 1;
+          break;
+          case one: char = key; skip = 0; break;
+        }
+        newtext += char;
+      } else break;
     }
   }
 }
