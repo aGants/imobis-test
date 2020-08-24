c1b1736300ffda082bf50cadc6bd045441adb142
Adelia <adelgants@gmail.com>
2020-08-24 22:35:11 +0300

    translate to english

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
+let check = document.querySelector('.checkbox');
+
 document.addEventListener('keyup', function (event) {
   let message = document.querySelector('.message');
   message = message.value;
-  console.log(message.length);
   document.querySelector('.symbols__number').innerHTML = message.length;
-})
+});
+
+check.addEventListener('change', function (event) {
+  if (check.checked == true) {
+    translate();
+  }
+});
+
+function translate() {
+  let message = document.querySelector('.message');
+  let text    = message.value;
+  let newtext = '';
+  const translit = {
+    'а': 'a',  'б': 'b',  'в': 'v',  'г': 'g',  'д': 'd', 
+    'е': 'e',  'ё': 'yo', 'ж': 'zh', 'з': 'z',  'и': 'i',
+    'й': 'y',  'к': 'k',  'л': 'l',  'м': 'm',  'н': 'n',  'о': 'o', 
+    'п': 'p',  'р': 'r',  'с': 's',  'т': 't',  'у': 'u',  'ф': 'f', 
+    'х': 'h',  'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'sch','ъ': "'",
+    'ы': 'i',  'ь': "'",  'э': 'e',  'ю': 'u',  'я': 'ya', 
+    '"': '«',  '»': '"',  '–': '-',  '—': '-',  '№': '#',  '`': "'"
+  };
+  for (var i=0; i < text.length; i++) {
+    newtext += translit[text.charAt(i)];
+  }
+  document.querySelector('.message').value = newtext;
+}
+
